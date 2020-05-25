f859cca96a83ca368234c03d3aabf9e1ea590421
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-05-25 15:49:31 +0200

    cleanup connections after shutdown

--- src/clj/datahike_server/database.clj
+++ src/clj/datahike_server/database.clj
@@ -2,9 +2,20 @@
   (:require [konserve.filestore :refer [new-fs-store]]
             [konserve.core :as k]
             [clojure.core.async :as async :refer [<!!]]
+            [datahike.api :as d]
             [mount.core :refer [defstate]]))
 
 (defstate database
   :start (atom {:connections {}
-                :store (<!! (new-fs-store "./.store"))}))
+                :store (<!! (new-fs-store "./.store"))})
+  :stop (do
+          (println "Cleaning up connections...")
+          (swap! database update :connections (fn [old]
+                                            (for [[id conn] old]
+                                              (d/release conn))))
+          (swap! database dissoc :connections)
+          (println "Done")
+          (println "Disconnecting from store...")
+          (swap! database dissoc :store)
+          (println "Done")))
 
